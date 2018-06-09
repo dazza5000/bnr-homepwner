1c720a9bf97d4166c0b46d99079c7e0788d27ce5
Darran Kelinske <darran.kelinske@gmail.com>
2018-06-09 18:27:33 -0500

    finish chapter 12

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Darran Kelinske on 6/9/18.
+//  Copyright © 2018 whereisdarran. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
